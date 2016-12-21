d72193f6a11111b0c65e8b641622f9c34fccb501
DavidMezaAstengo <sda>
2016-12-21 05:45:43 -0300

    Added: Post de positivo, negativo y warning

--- src/routes.php
+++ src/routes.php
@@ -1,6 +1,7 @@
 <?php
 // Routes
 
+// nombre servidor
 $app->get('/[{name}]', function ($request, $response, $args) {
     $this->logger->info("Slim-Skeleton '/' route");
     return $this->renderer->render($response, 'index.phtml', $args);
@@ -17,9 +18,16 @@ class Pic extends Illuminate\Database\Eloquent\Model {
     public $timestamps = false;
 }
 /*
- * hacer twin
+ * twin con los id de local y remota
  */
+class Twin extends Illuminate\Database\Eloquent\Model {
 
+    protected $table = 'Twin';
+    //relleno massivo
+    protected $fillable = ['idLocal','idRemota'];
+    //eliminar tablas created_at updated_at
+    public $timestamps = false;
+}
 /*
 class Order extends Illuminate\Database\Eloquent\Model {
 
@@ -27,52 +35,79 @@ class Order extends Illuminate\Database\Eloquent\Model {
     public $timestamps = false;
 }
 */
-
+//insertar pic
 $app->post('/insert/pic',function(){
     $json = file_get_contents('php://input');
     $pic = json_decode($json,true);
     $conexion = mysqli_connect('localhost','root','','pictwin');
-    $consulta = "INSERT INTO Pic (deviceId,date,latitud,longitud,positivo,negativo,warning,pic) VALUES 
-('".$pic['deviceId']."','".$pic['date']."','".$pic['latitud']."','".$pic['longitud']."',0,0,0,1,'".$pic['pic']."')";
+
+    $consulta = "INSERT INTO Pic (deviceId,date,latitud,longitud,positivo,negativo,warning,pic) VALUES  
+                  ('".$pic['deviceId']."','".$pic['date']."','".$pic['latitud']."','".$pic['longitud']."',
+                  0,0,0,1,'".$pic['pic']."')";
+
     $conexion->query($consulta);
     mysqli_close($conexion);
 
 });
 
 $app->post('/get/pic', function (){
-    $data = file_get_contents('php://input');
-    $pic = json_decode($data, true);
 
     $conexion = mysqli_connect('localhost', 'root', '', 'pictwin');
-      $consulta = " SELECT
-  Pic.*
-FROM
-  Twin, Pic
-WHERE
-  Twin.remote_pic_id = Pic.id
-GROUP BY Pic.id
-HAVING count(Pic.id) = (
-    SELECT count(Pic.id) AS count
-  FROM
-    Twin, Pic
-  WHERE
-    Twin.remote_pic_id = Pic.id
-  GROUP BY Pic.id
-  ORDER BY count
-  LIMIT 1);";
-
+      $consulta = " SELECT Pic.*
+                    FROM Twin, Pic
+                    WHERE Twin.remote_pic_id = Pic.id
+                    GROUP BY Pic.id
+                    HAVING count(Pic.id) = (
+                    SELECT count(Pic.id) AS count
+                    FROM Twin, Pic
+                    WHERE Twin.remote_pic_id = Pic.id
+                    GROUP BY Pic.id
+                    ORDER BY count
+                    LIMIT 1)";
     $resultado = $conexion->query($consulta);
-
-    /*
- * hacer update de el cont
- */
-/*
- * hacer el twin, me falta el id de la primera
- */
     $arreglo = $resultado->fetch_assoc();
-
     mysqli_close(conexion);
     $json = json_encode($arreglo);
     return $json;
 });
 
+
+
+$app->post('/insert/positivo',function(){
+    $json = file_get_contents('php://input');
+    $pic = json_decode($json,true);
+    $conexion = mysqli_connect('localhost','root','','pictwin');
+
+    $consulta = "UPDATE Pic SET positive= positive + 1 WHERE id = '".$pic['dbid']."'";
+
+    $conexion->query($consulta);
+    mysqli_close($conexion);
+
+
+});
+
+$app->post('/insert/negativo',function(){
+    $json = file_get_contents('php://input');
+    $pic = json_decode($json,true);
+    $conexion = mysqli_connect('localhost','root','','pictwin');
+
+    $consulta = "UPDATE Pic SET negativo = negativo + 1 WHERE id = '".$pic['dbid']."'";
+
+    $conexion->query($consulta);
+    mysqli_close($conexion);
+
+
+});
+
+$app->post('/insert/warning',function(){
+    $json = file_get_contents('php://input');
+    $pic = json_decode($json,true);
+    $conexion = mysqli_connect('localhost','root','','pictwin');
+
+    $consulta = "UPDATE Pic SET warning = warning + 1 WHERE id = '".$pic['dbid']."'";
+
+    $conexion->query($consulta);
+    mysqli_close($conexion);
+
+
+});
